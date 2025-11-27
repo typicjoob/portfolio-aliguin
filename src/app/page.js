277f9b3b24ed72b636b0131e71f233c67655146a
typicjoob <aliguinjoevany123@gmail.com>
2025-11-27 14:06:33 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import NavigationBar from "./components/NavigationBar";
 import LandingHeroSection from "./components/LandingHeroSection";
 import AboutMeSection from "./components/AboutMeSection";
@@ -10,12 +9,14 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black-">
       <NavigationBar />
-      <div className="container mt-24 mx-auto px-12 bg-linear-to-b sm:bg-linear-to-r from-black via-gray-900 to-amber-800">
-        <LandingHeroSection />
-        <AboutMeSection />
-        <SkillsAndExpertiseSection />
-        <ProjectsShowcase />
-        <ContactSection />
+      <div className="bg-linear-to-b sm:bg-linear-to-r from-black via-gray-900 to-amber-800">
+        <div className="container mt-24 mx-auto px-12">
+          <LandingHeroSection />
+          <AboutMeSection />
+          <SkillsAndExpertiseSection />
+          <ProjectsShowcase />
+          <ContactSection />
+        </div>
       </div>
     </main>
   );
